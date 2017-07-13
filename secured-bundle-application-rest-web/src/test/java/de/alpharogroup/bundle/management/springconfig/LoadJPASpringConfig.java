19d8382337d10980fc90541fd748cfd62e01efd1
Asterios Raptis <asterios.raptis@gmx.net>
2017-07-13 09:02:36 +0200

    update parent version

--- secured-bundle-application-rest-web/src/test/java/de/alpharogroup/bundle/management/springconfig/LoadJPASpringConfig.java
+++ secured-bundle-application-rest-web/src/test/java/de/alpharogroup/bundle/management/springconfig/LoadJPASpringConfig.java
@@ -29,13 +29,17 @@ import org.springframework.context.annotation.*;
 
 import de.alpharogroup.db.resource.bundles.service.api.BundleApplicationsService;
 
-public class LoadJPASpringConfig {
+public class LoadJPASpringConfig
+{
 
 
-   public static void main(final String[] args) {
-	   final ApplicationContext ctx = new AnnotationConfigApplicationContext(PersistenceJPAConfig.class);
-	   final BundleApplicationsService bundleApplicationsService = ctx.getBean(BundleApplicationsService.class);
-	   bundleApplicationsService.findAll();
-
+	@SuppressWarnings("resource")
+	public static void main(final String[] args)
+	{
+		final ApplicationContext ctx = new AnnotationConfigApplicationContext(
+			PersistenceJPAConfig.class);
+		final BundleApplicationsService bundleApplicationsService = ctx
+			.getBean(BundleApplicationsService.class);
+		bundleApplicationsService.findAll();
 	}
 }
